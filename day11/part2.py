e89781799f218250ad3388ee403c9c2ac0043032
vilewrath <alexanderhjpettifer@gmail.com>
2021-09-28 22:57:18 +0100

    Added day11 part 2 and cleaned part 1

---
+++ day11/part2.py
@@ -0,0 +1,47 @@
+import part1
+
+
+class SightBoard(part1.GameBoard):
+
+    def rule(self, x, y):
+        floor = '.'
+        occupied = '#'
+        empty = 'L'
+
+        current_cell = self.arr[x][y]
+        if current_cell == floor:
+            return floor
+
+        occupied_count = 0
+        for dx, dy in self.directions:
+            sub_x, sub_y = x + dx, y + dy
+
+            while sub_y not in (-1, self.max_y) and \
+                    sub_x not in (-1, self.max_x):
+                cell = self.arr[sub_x][sub_y]
+                if cell == floor:
+                    sub_x += dx
+                    sub_y += dy
+                elif cell == occupied:
+                    occupied_count += 1
+                    break
+                else:
+                    break
+
+        if occupied_count >= 5:
+            return empty
+        elif current_cell == empty and occupied_count == 0:
+            return occupied
+        else:
+            return current_cell
+
+
+if __name__ == '__main__':
+    board = SightBoard()
+    board.load_board("input")
+
+    flag = True
+    while flag:
+        flag = board.update()
+
+    print(board.count_occupied())
